99182818d119f602387ff1501e4812095edee33e
AL2002MI08 <alexandramizero@gmail.com>
2022-09-19 03:59:49 -0700

    simple rectsngle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sept 19 2022
+@author: Alex
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
