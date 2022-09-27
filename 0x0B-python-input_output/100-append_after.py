d78385b0de511686d4b87bc294dd8cf68ab37efa
AL2002MI08 <alexandramizero@gmail.com>
2022-09-27 11:15:34 -0700

    python input output advanced task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a append_after function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file after
+        each line containing a specific string
+    Args:
+        filename (str): The name of the file
+        search_string (str): The string to math
+        new_string (str): The string to insert after matching
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for i in range(len(lines)):
+        if search_string in lines[i]:
+            lines.insert(i + 1, new_string)
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        content = "".join(lines)
+        file.write(content)
