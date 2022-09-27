71a5782f0af205382f1a2e921a45d196e55d526e
AL2002MI08 <alexandramizero@gmail.com>
2022-09-27 10:35:43 -0700

    writes file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    Counts the number of lines from the filename
+    Arguments:
+        filename (str): The name of the file to count in
+    """
+    with open(filename, encoding='utf-8') as file:
+        lines = file.readlines()
+    return len(lines)
